477aa0090b25b65fd6e85ef1c3930f98f3e1b323
RicardoClaros <ricardoclaros25@gmail.com>
2017-04-24 14:56:52 -0700

    Search function

---
+++ js/Main.js
@@ -0,0 +1,20 @@
+// The function and event listener
+function bookSearch(){
+  console.log('this function runs!')
+}
+
+document.getElementById('button').addEventlistener('click', bookSearch, false
+
+ // Getting user data and clearing the previous search
+ function bookSearch(){
+   var search= document.getElementById("search").value
+   document.getElementById("results").innerHTML = ""
+   console.log(search)
+ }
+
+ // Setup the AJAX call for the API data based on the user's search term
+ function bookSearch(){
+   ...
+
+   $.AJAX
+ }
